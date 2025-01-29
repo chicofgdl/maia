6085a83d1bf5be9f2e5028640ea94fca685eab6e
Francisco <fglb@cin.ufpe.br>
2025-01-29 07:56:01 -0300

    Adiciona funcionalidade de armazenar e exibir mensagens do usuário - feitos ajustes no layout - resposta do bot e armazenamento das mensagens do bot pendentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,19 @@
 'use client';  // Pasta app renderiza do lado do servidor. Essa linha informa que é para renderizar do lado cliente.
-import ChatInput from '@/components/ChatInput';
 import Sidebar from '../components/Sidebar';
 import Checklist from '@/components/Checklist';
-import { useState } from 'react';
+import Chatbot from '@/components/Chatbot';
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    { type: 'user', text: 'Olá, o que você faz?' },
-    { type: 'bot', text: 'Eu sou um chatbot que responde perguntas sobre sua empresa!' },
-  ]);
-
-  // Adicionar uma nova mensagem (exemplo com mock)
-  const addMessage = (message: string, type: 'user' | 'bot') => {
-    setMessages((prev) => [...prev, { type, text: message }]);
-  };
+  
 
   return (
-    <div className="w-full h-screen flex bg-gray-600 p-6">
+    <div className="w-full h-screen flex bg-gray-600 p-6 gap-6">
 
-      <div className="w-1/4 bg-gray-400 p-6 rounded-2xl">
+      <div className="w-1/5 bg-gray-400 p-6 rounded-2xl">
         <Sidebar />
       </div>
-      <div className="flex-1 flex flex-col px-6 gap-6">
-        {/* Histórico de Conversa */}
-        <div className="h-[85%] bg-gray-400 rounded-2xl p-6 overflow-y-auto">
-          <div className="h-full w-full bg-gray-300 rounded-2xl p-8 flex flex-col gap-4">
-            {messages.map((msg, index) => (
-              <div
-                key={index}
-                className={`p-3 rounded-lg ${
-                  msg.type === 'user' ? 'bg-blue-500 self-end text-white' : 'bg-gray-200 self-start'
-                }`}
-              >
-                {msg.text}
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Campo de Input */}
-        <div className="flex-1 p-4 bg-gray-400 rounded-2xl">
-          <ChatInput
-            onSubmit={(message: string) => addMessage(message, 'user')} // Exemplo: Adiciona mensagem do usuário
-          />
-        </div>
+      <div className="w-2/4 bg-gray-400 p-6 rounded-2xl">
+        <Chatbot/>
       </div>
       <div className="w-1/4 bg-gray-100 p-6 rounded-2xl">
         <Checklist/>
---
+++ src/components/Chatbot/index.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+export default function Chatbot() {
+  const [messages, setMessages] = useState([
+    { type: "user", text: "Olá, o que você faz?" },
+    {
+      type: "bot",
+      text: "Eu sou um chatbot que responde perguntas sobre sua empresa!",
+    },
+    { type: "user", text: "Olá, o que você faz?" },
+    {
+      type: "bot",
+      text: "Eu sou um chatbot que responde perguntas sobre sua empresa!",
+    },
+  ]);
+
+  // Adicionar uma nova mensagem (exemplo com mock)
+  const addMessage = (message: string, type: "user" | "bot") => {
+    setMessages((prev) => [...prev, { type, text: message }]);
+  };
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+  };
+
+  return (
+    <div className="flex-1 flex flex-col gap-6 h-full">
+      {/* Histórico de Conversa */}
+      <div className="h-full rounded-2xl overflow-y-auto">
+        <div className="h-full w-full bg-gray-300 rounded-2xl p-8 flex flex-col gap-4">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`p-3 rounded-lg ${
+                msg.type === "user"
+                  ? "bg-blue-500 self-end text-white"
+                  : "bg-gray-200 self-start"
+              }`}
+            >
+              {msg.text}
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Campo de Input */}
+      <div className="flex-1 flex-row h-full gap-6 bg-gray-400 rounded-2xl w-full h-full">
+        <div className="flex flex-row h-full gap-6">
+          <input
+            className="w-full rounded-2xl p-6 bg-gray-300"
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Pergunte sobre o banco de dados"
+          />
+          <button type="button" onClick={() => addMessage(inputValue, "user")}>
+            <div className="bg-blue-300 w-24 h-full rounded-2xl flex items-center justify-center">
+              Buscar
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
