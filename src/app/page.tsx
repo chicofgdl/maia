02a1449e38af2595ad1a087aace88b2b4f32fa9d
Francisco Gabriel <francisco_abada@hotmail.com>
2025-02-24 20:58:16 -0300

    Adiciona página de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-'use client';  // Pasta app renderiza do lado do servidor. Essa linha informa que é para renderizar do lado cliente.
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Sidebar from '../components/Sidebar';
 import Checklist from '@/components/Checklist';
 import Chatbot from '@/components/Chatbot';
 
 export default function Home() {
   
+  const router = useRouter();
+  
+      useEffect(() => {
+          console.log("Login useEffect");
+          console.log("token: " + localStorage.getItem("token"));
+          const isAuthenticated = localStorage.getItem("token");
+          if (!isAuthenticated) {
+              router.push("/login");
+          }
+      }, [router]);
 
   return (
     <div className="w-full h-screen flex bg-blue-900 p-6 gap-6 justify-center">
