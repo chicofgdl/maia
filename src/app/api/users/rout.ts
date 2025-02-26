befc89250c70dffa372c7203cb5aa1255ac9669f
fagpaf <fagpaf@cin.ufpe.br>
2025-02-25 21:31:49 -0300

    adicionando o dataset e as rotas da api

---
+++ src/app/api/users/rout.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password, role } = await req.json();
+
+    // Validação básica
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "Nome, e-mail e senha são obrigatórios" },
+        { status: 400 }
+      );
+    }
+
+    // Hash da senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Cria o usuário
+    const user = await prisma.user.create({
+      data: { name, email, password: hashedPassword, role },
+    });
+
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao criar usuário" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao buscar usuários" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DEL(){
+
+}
